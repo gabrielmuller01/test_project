e3e435402a4a085fff3db20a92804cb6ac8deeaa
Shanzoo <90108732+Shanzoo@users.noreply.github.com>
2022-08-18 17:04:08 -0300

    complete-front

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 var pessoas = []
+var pessoasJSON = {pessoas}
 var element = document.querySelector(".family");
 var include = document.getElementById('name')
 
+document.onload(callJson())
 
 function addPerson(name) {
 
@@ -12,15 +14,21 @@ function addPerson(name) {
     element.innerHTML +=
     (
     `
-    <tr style="max-widht: 300px; display: inline-block; text-align: center;">
-        <td style="text-align:center; width: 200px; background-color: #848482;">
+    <tr style="display: inline-block; text-align: center;">
+        <td style="text-align:center; width: 201px; background-color: #848482;">
             ${pessoa.nome}
         </td>
 
-        <td style="text-align:center; width: 100px; background-color:#848482;">
+        <td style="text-align:center; width: 101px; background-color:#848482;">
             <button type="button"  onclick="removePerson(this)"> Remover</button>
         </td>
+        <td style="display: flex; justify-content: center;">
+            <table>
+                <tbody>
 
+                </tbody>
+            </table>
+        </td>
         <td style="text-align:center; display: inline-block;">
             <button type="button" onclick="addChildPerson(this)"> Adicionar Filho</button>
         </td>
@@ -33,31 +41,24 @@ function addPerson(name) {
 }
 
 function addChildPerson(pessoa) {
-    
+
     var sonName = prompt("Digite o nome do filho: ");
     var closestTr = pessoa.closest('tr');
     var array = Array.from(element.rows);
     var i = array.indexOf(closestTr);
 
     pessoas[i].filhos.push(sonName)
-
-    var table = document.createElement('table'); 
-    var td = document.createElement('td');
-    td.appendChild(table)
-    closestTr.insertCell(2).appendChild(td) 
-
-    table.innerHTML += 
+    console.log(closestTr.cells[2].children)
+    closestTr.cells[2].children[0].children[0].innerHTML += 
     `<tr>
-        <td style=" background-color: #B6B6B4; width: 200px; text-align: left;">
+        <td style=" background-color: lightgray; width: 200px;">
             ${sonName}
         </td>
-        <td style=" background-color: #B6B6B4; widht: 100px">
+        <td style=" background-color: lightgray; width: 100px;">
             <button type="button" onclick="removeSon(this)"> Remover Filho</button>
         </td>
     </tr>`;
-
-    closestTr.cells[2].style.cssText = 'display: flex'
-
+    
     callJson();
 }
 
@@ -77,23 +78,23 @@ function removePerson(removeButton) {
 
 function removeSon(removeChildButton) {
 
-    var closestTable = removeChildButton.closest('table');
-    var closestTr = removeChildButton.closest('td');
-    
-    var closestFather = closestTable.closest('tr')
-    var closestFatherarray = Array.from(element.rows)
-    var j = closestFatherarray.indexOf(closestFather)
+    var closestTr = removeChildButton.closest('tr');
 
-    var array = Array.from(closestTable.rows);
+    var table = removeChildButton.closest('table')
+
+    var paiTr = table.parentNode.parentNode
+
+    var arrayPai = Array.from(element.rows);
+
+    var p = arrayPai.indexOf(paiTr)
+
+    var array = Array.from(table.rows);
+
     var i = array.indexOf(closestTr);
-
-    var findFather = removeChildButton.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode;
-
-    //console.log(findFather)
-
-    pessoas[j].filhos.splice(i, 1)
     
-    findFather.remove();
+    table.rows[i].remove();
+
+    pessoas[p].filhos.splice(i, 1)
 
     callJson()
 }
@@ -102,16 +103,7 @@ function callJson(){
 
     var textArea = document.getElementById('TextArea')
 
-    textArea.value = JSON.stringify(pessoas)
+    textArea.value = JSON.stringify(pessoasJSON, undefined, 8)
 }
-
+callJson();
 console.log(include.value)
-
-// var Joao = addPerson('Joao')
-// var Will = addPerson('Will')
-// var Rena = addPerson('Rena')
-
-console.log(JSON.stringify(pessoas))
-
-
-
